3a8057949f50db1d99fcb2d2e80eb870b106aa9c
gerson.carrasco <gerson.carrasco@gestionysistemas.com>
2021-02-10 12:40:48 -0500

    Patron observer

---
+++ src/main/java/patterns/observer/Observer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer.pattern.demo;
+import java.util.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Observer {
+    public void update() {
+        System.out.println("flag value changed in Subject");
+    }
+}
