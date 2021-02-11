4c1aa1882c515795d2c7c31466e77f10e94b5aa4
Cesar Gutierrez <a20062161@pucp.edu.pe>
2021-02-11 17:43:23 -0500

    Add try catch

--- src/main/java/servlets/MessagingServlet.java
+++ src/main/java/servlets/MessagingServlet.java
@@ -13,7 +13,8 @@ import java.io.IOException;
 
 @WebServlet(name = "MessagingServlet", displayName = "MessagingServlet", urlPatterns = {"/messages"}, loadOnStartup = 1)
 public class MessagingServlet extends HttpServlet {
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         User currentUser = new User();
         String messageText = request.getParameter("messageText");
         HttpSession session = request.getSession();
@@ -24,15 +25,22 @@ public class MessagingServlet extends HttpServlet {
         Message message = new Message();
         message.setSender(currentUser);
         message.setText(messageText);
-
-        request
-                .getRequestDispatcher("/index.jsp")
-                .forward(request, response);
+        try {
+            request
+                    .getRequestDispatcher("/index.jsp")
+                    .forward(request, response);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request
-                .getRequestDispatcher("/index.jsp")
-                .forward(request, response);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException  {
+        try {
+            request
+                    .getRequestDispatcher("/index.jsp")
+                    .forward(request, response);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
     }
 }
